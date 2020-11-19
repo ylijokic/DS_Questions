d65ed95ab03b9ca3a37edc4dfa9a5c0a6afe49f4
Charlie <ylijokic@oregonstate.edu>
2020-11-19 13:19:42 -0800

    Upload Python/Notebook for Question 2B

--- question2B.py
+++ question2B.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_csv('./data.csv', names=['index', 'value'])
 
+# Descriptive Stats
 mean = df['value'].mean()
 print(mean)
 
@@ -18,16 +20,30 @@ print(median)
 std = df['value'].std()
 print(std)
 
+# Summary of Descriptive Stats
 print('---------------------------')
 
 print(df['value'].describe())
 
 print('---------------------------')
 
+# Display Histogram
 df.plot(x = 'index', y = 'value', kind = 'hist')
 plt.show()
 
 print('---------------------------')
+
+# Display skew of values 
+print(df['value'].skew())
+
+# The data is skewed to the right so we can cap the higher values.
+# We can get the 90th percentile of the values and cap the data there
+print(df['value'].quantile(0.90))
+df["value"] = np.where((df["value"] > 26.9) & (df["value"] < 19.2), 26.9 , df['value'])
+print(df['value'].skew())
+
+df.plot(x = 'index', y = 'value', kind = 'hist')
+plt.show()
 
 
 
