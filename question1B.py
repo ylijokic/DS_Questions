1e79c10ed49e33c521a72eeb48747728780b9f4b
Charlie <ylijokic@oregonstate.edu>
2020-11-18 16:20:29 -0800

    Upload Python Code for Question 1-B

---
+++ question1B.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import sqlalchemy
+
+engine = sqlalchemy.create_engine('mysql://root:@localhost/ds_questions')
+df1 = pd.read_sql_table('name_table', engine)
+df2 = pd.read_sql_table('mark_table', engine)
+    
+def change_names(data_frame):
+    
+    # Create DF copy and iterate through 'Student_Name' col using index method
+    new_df = data_frame.copy(deep=True)
+    
+    for index in new_df.index:
+        name = new_df['Student_Name'][index]
+        
+        # Conditional to make manipulate name
+        if ('e' in name or 'E' in name):
+            new_df['Student_Name'][index] = name.upper()
+        else:
+            new_df['Student_Name'][index] = name.lower()
+    
+    return new_df
+
+
+df3 = change_names(df1)
+
+for index in df3.index:
+    print(df3['Student_Name'][index])
+
