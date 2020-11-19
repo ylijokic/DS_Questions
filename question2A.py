50b169c31ddba3020adb030a05f8ef48a4f1712b
Charlie <ylijokic@oregonstate.edu>
2020-11-19 09:58:08 -0800

    Upload csv File and Python code for Question 2-A

---
+++ question2A.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('./data2.csv')
+
+# Print the Shape of the DataFrame to see number of expected rows & columns
+print('---------------------------')
+
+print(df.shape)
+
+# Print the DataFrame info to see what columns have missing data
+print('---------------------------')
+
+print(df.info())
+
+print('---------------------------')
+
+print(df.apply(lambda x: sum(x.isnull()), axis=0))
+
+# The Position Title has 2 Null Values, so we need to account for that
+# Looking at the CSV File we can see that there are two rows where the Position Title got appended to the Pay Basis
+print('---------------------------')
+# Search for the rows where the Pay Basis is not "Per Annum"
+for index in df.index:
+        pay_basis = df['PAY BASIS'][index]
+        if (pay_basis != "Per Annum"):
+            # Slice the pay_basis string to extract the actual job title
+            job_title = pay_basis[10:]
+
+            # Fill in the correct pay_basis and position_title for this specific index
+            df['PAY BASIS'][index] = pay_basis[:10]
+            df['POSITION TITLE'][index] = job_title
+
+print('---------------------------')
+
+print(df)
+
+
